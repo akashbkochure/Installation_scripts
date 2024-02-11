b5081c6bba035f6444f2e4f65fd439e45fa3d652
Akash Bharat Kochure <115985398+akashbkochure@users.noreply.github.com>
2024-02-11 18:50:51 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt install openjdk-17-jre -y
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee \
-  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-sudo apt-get install jenkins -y
+sudo apt-get update -y && sudo apt-get upgrade -y
+sudo apt install openjdk-11-jdk
+java --version
+curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/" | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install -y jenkins
 sudo systemctl start jenkins
+sudo systemctl enable jenkins
 sudo systemctl status jenkins
